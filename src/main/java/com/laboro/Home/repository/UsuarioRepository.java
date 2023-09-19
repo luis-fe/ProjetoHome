e6f04764c35ae663e3f371dba11728c2e6409173
Lucas Martins de Oliveira <lucasm236954@gmail.com>
2023-09-18 21:29:01 -0300

    create UsuarioRepository

--- src/main/java/com/laboro/Home/repository/UsuarioRepository.java
+++ src/main/java/com/laboro/Home/repository/UsuarioRepository.java
@@ -1,8 +1,13 @@
 package com.laboro.Home.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.laboro.Home.model.Usuario;
+
 @Repository
-public interface UsuarioRepository {
-    
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+    List<Usuario> findByNomeAndAtivo(String nome, Boolean status);
 }
